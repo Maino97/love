5c88389e7d65f68ff40f359d47193245f7330216
Maino97 <medaminewahi97@gmail.com>
2022-05-31 18:16:58 +0200

    initial commit

---
+++ src/redux/reducers/pokemon.js
@@ -0,0 +1,27 @@
+/* eslint-disable import/no-anonymous-default-export */
+import { GET_POKEMONS, GET_POKEMON } from '../constants/actionTypes';
+
+const initialState = {
+  pokemons: [],
+  pokemon: {},
+  loading: true,
+};
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case GET_POKEMONS:
+      return {
+        ...state,
+        pokemons: action.payload,
+        loading: false,
+      };
+    case GET_POKEMON:
+      return {
+        ...state,
+        pokemon: action.payload,
+        loading: false,
+      };
+    default:
+      return state;
+  }
+}
